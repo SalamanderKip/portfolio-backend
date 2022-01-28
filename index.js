e53c44823d88d48d124efd3c14bfdb85118fb384
Maarten Bos <200507@student.glu.nl>
2022-01-28 20:32:14 +0100

    backend done

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const app = express()
 const db = require('./config')
+db.connection.connect();
+
 app.get('/projects', function (req, res) {
-    db.connection.connect();
-
-    db.connection.query('SELECT * FROM projecten', function (error, results, fields) {
-        if (error) throw error;
-        var data = JSON.stringify(results)
-        console.log(data);
-        return res.send(data)
-    });
-
-    db.connection.end();
+    db.connection.query("SELECT * FROM projecten",(error,result,fields)=>{
+        console.log(result)
+        res.send(result).status(200)
+    })
 })
 
+// app.get("/test", (res,req)=> {
+//   db.connection.query("SELECT * FROM projecten",(error,result,fields)=>{
+//       console.log(result)
+//       req.send(result).status(200)
+//   })
+// })
 app.listen(3001)
